422a105a7de5c0f9a94d5069a8a22cfbfa6c4f02
Lucas Costa <lucas.co090869@gmail.com>
2025-07-19 18:44:15 -0300

    feat: Refactor EventsPage to use loader function for data fetching

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,11 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { EditEventPage } from './pages/EditEventPage';
 import { EventsDetailPage } from './pages/EventDetailPage';
-import { EventsPage } from './pages/EventsPage';
+import { EventsPage, loader as eventsLoaderFn } from './pages/EventsPage';
 import { EventsRootLayout } from './pages/EventsRootLayout';
 import { HomePage } from './pages/HomePage';
 import { NewEventPage } from './pages/NewEventPage';
 import { RootLayout } from './pages/RootLayout';
-
-// Challenge / Exercise
-
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// 4. Add properly working links to the MainNavigation
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-// 7. Output the ID of the selected event on the EventDetailPage
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
 const router = createBrowserRouter([
   {
@@ -42,16 +20,7 @@ const router = createBrowserRouter([
           {
             index: true,
             element: <EventsPage />,
-            loader: async () => {
-              const response = await fetch('http://localhost:8080/events');
-
-              if (!response.ok) {
-                // TODO: Handle error response
-              } else {
-                const resData = await response.json();
-                return resData.events;
-              }
-            },
+            loader: eventsLoaderFn
           },
           { path: ':eventId', element: <EventsDetailPage /> },
           { path: 'new', element: <NewEventPage /> },
--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -1,10 +1,19 @@
-import { useEffect, useState } from 'react';
-
-import EventsList from '../components/EventsList';
 import { useLoaderData } from 'react-router-dom';
+import EventsList from '../components/EventsList';
 
 export function EventsPage() {
   const events = useLoaderData();
 
   return <EventsList events={events} />;
 }
+
+ export async function loader() {
+  const response = await fetch('http://localhost:8080/events');
+
+  if (!response.ok) {
+    // TODO: Handle error response
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+};
