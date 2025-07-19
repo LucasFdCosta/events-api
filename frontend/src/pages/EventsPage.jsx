6f09bebfdf8abc68f37e9010d8fd25f16af32e6f
Lucas Costa <lucas.co090869@gmail.com>
2025-07-19 18:36:35 -0300

    feat: Implement data fetching for events in EventsPage component
    - Implement data fetching for events in EventsPage component using the loader() function from react-router

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -1,22 +1,10 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
-const EVENTS = [
-  { id: 1, title: 'Event 1' },
-  { id: 2, title: 'Event 2' },
-];
+import EventsList from '../components/EventsList';
+import { useLoaderData } from 'react-router-dom';
 
 export function EventsPage() {
-  return (
-    <div>
-      <h1>Events Page</h1>
-      <p>Welcome to the Events Page!</p>
-      <ul>
-        {EVENTS.map(event => (
-          <li key={event.id}>
-            <Link to={`${event.id}`}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  const events = useLoaderData();
+
+  return <EventsList events={events} />;
 }
